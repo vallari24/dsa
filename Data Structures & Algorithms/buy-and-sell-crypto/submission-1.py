8d8380565d16ee487d7228ce07571c407a8a3df6
Vallari Mehta <vallari.mehta@gmail.com>
2026-05-19 05:35:28 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n,maxprofit = len(prices),0
+        minprice = prices[0]
+        
+        for i in range(1,n):
+            curprofit = prices[i] - minprice
+            maxprofit = max(maxprofit,curprofit)
+            minprice = min(minprice,prices[i])
+        return maxprofit
+#[10,1,5,6,7,1], minprice = 10
+# 1-10 , minprice = 1
+# 5-1 = 4
+
+
+    #    for i in range(n):
+    #         for j in range(i+1,n):
+    #             curprofit = prices[j] - prices[i]
+    #             maxprofit = max(maxprofit,curprofit)
+        
+    #     return maxprofit
+
+# prices = [10,1,7,2,20,0,30]
+# 1-10 =-1, 7-10= -3 , 2-10
+# 7-1,2-1
+# 2-7
+# 1
+
+
+# prices = [10,8,7,5,2]
+# 8-10, 7-10, 5-10, 2-19
+# 7-8, 5-8, 2-8
+# 5-7, 2-7
+# 2-5
+
+# 0
+# 30   
